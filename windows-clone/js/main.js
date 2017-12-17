469b81a64ec83ba5d26f313d68af71f959c14f65
Curt <34591397+nomadevs@users.noreply.github.com>
2017-12-17 15:49:51 -0800

    Add files via upload

---
+++ windows-clone/js/main.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+
+  $("#myFolder,#myTrash").modal({
+    backdrop: false,
+	show: false
+  });
+  $("#myFolder,#myTrash").draggable();
+  $(".btn-computer,.btn-trash,.btn-folder,.btn-facebook,.btn-twitter").draggable();
+  
+  // Toggle volume
+  $("#volume").on("click", function() {
+    var $this = $(this);
+    if ( $this.hasClass("fa fa-volume-up") ) {
+      $this.removeClass("fa fa-volume-up");
+	  $this.addClass("fa fa-volume-off");
+	} else {
+      $this.removeClass("fa fa-volume-off")
+      $this.addClass("fa fa-volume-up");
+	}
+  });
+
+});
